bde2be3460bce28e100de2692d8fda0b34fff5fd
Anton Ekström <anton.e.ekstrom@gmail.com>
2021-09-04 21:06:42 +0200

    Adds javafx application boilerplate

--- src/main/java/andioopp/AndIOOPP.java
+++ src/main/java/andioopp/AndIOOPP.java
@@ -1,7 +1,22 @@
-package main.java.andioopp;
+package andioopp;
 
-public class AndIOOPP {
-	public static void main(String args[]){
-		System.out.println("And I OOPP :)");
+import andioopp.gfx.WindowBuilder;
+import andioopp.gfx.javafx.FxWindowBuilder;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.awt.*;
+
+public class AndIOOPP extends Application {
+	public static void main(String[] args) {
+		AndIOOPP.launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) {
+		WindowBuilder builder = new FxWindowBuilder(stage);
+		builder.setTitle("and I OOPP");
+		builder.setSize(new Dimension(128, 128));
+		builder.build();
 	}
 }
