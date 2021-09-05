b809238aa109d0f9fe63088d68da24fc8a4b2521
Anton Ekström <anton.e.ekstrom@gmail.com>
2021-09-05 17:12:29 +0200

    Changes generic types for renderers to hopefully work better

--- src/main/java/andioopp/gfx/javafx/FxRenderer.java
+++ src/main/java/andioopp/gfx/javafx/FxRenderer.java
@@ -4,9 +4,8 @@ import andioopp.gfx.Renderer;
 import andioopp.gfx.Sprite;
 import javafx.geometry.Point2D;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.Image;
 
-public class FxRenderer implements Renderer<Sprite<? extends Image>> {
+public class FxRenderer implements Renderer<FxSprite> {
 
     private final GraphicsContext ctx;
 
@@ -15,7 +14,7 @@ public class FxRenderer implements Renderer<Sprite<? extends Image>> {
     }
 
     @Override
-    public void drawSprite(Sprite<? extends Image> sprite) {
+    public void drawSprite(FxSprite sprite) {
         Point2D p = sprite.getPosition();
         getCtx().drawImage(sprite.getImage(),  p.getX(), p.getY());
     }
