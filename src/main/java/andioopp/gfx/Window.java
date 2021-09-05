0a58363a73941a9242d9599ae9c9079fbe8824be
Anton Ekström <anton.e.ekstrom@gmail.com>
2021-09-05 17:59:37 +0200

    Adds documentation for gfx package

--- src/main/java/andioopp/gfx/Window.java
+++ src/main/java/andioopp/gfx/Window.java
@@ -1,5 +1,13 @@
 package andioopp.gfx;
 
+/**
+ * Represents the application window.
+ * @param <R> Describes the type of {@link Renderer} the window has
+ */
 public interface Window<R extends Renderer<?>> {
+    /**
+     * Creates a {@link Renderer}.
+     * @return The {@link Renderer}
+     */
     R getRenderer();
 }
