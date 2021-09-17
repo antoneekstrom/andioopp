150786a34ed7d24cbfea6c516b299135deb5763c
Anton Ekström <anton.e.ekstrom@gmail.com>
2021-09-17 21:27:13 +0200

    Removes dead code and refactors

--- src/main/java/andioopp/domain/model/Lane.java
+++ src/main/java/andioopp/domain/model/Lane.java
@@ -1,24 +1,17 @@
 package andioopp.domain.model;
 
-import andioopp.common.time.Time;
 import andioopp.domain.model.enemy.Enemy;
-import andioopp.domain.model.tower.Tower;
 
 import java.util.Collection;
 import java.util.List;
 
-public class Lane implements Updateable {
+public class Lane {
     private final List<Cell> cells;
     private final Collection<Enemy> enemies;
 
     Lane(List<Cell> cells, Collection<Enemy> enemies) {
         this.cells = cells;
         this.enemies = enemies;
-    }
-
-    @Override
-    public void update(Time time) {
-
     }
 
     public static Cell getCell(List<Lane> lanes, int row, int col) {
--- src/main/java/andioopp/domain/model/World.java
+++ src/main/java/andioopp/domain/model/World.java
@@ -1,44 +1,22 @@
 package andioopp.domain.model;
 
 import andioopp.common.time.Time;
-import andioopp.domain.model.enemy.Enemy;
-import andioopp.domain.model.tower.TargetingStrategy;
-import andioopp.domain.model.tower.Tower;
 
-import java.util.Collection;
 import java.util.List;
 
 public class World implements Updateable {
+
     private final List<Lane> lanes;
 
     World(List<Lane> lanes) {
         this.lanes = lanes;
     }
 
-    public void placeTower(Tower tower, int row, int col) {
-        Lane.getCell(getLanes(), row, col).setTower(tower);
+    @Override
+    public void update(Time time) {
     }
 
     public List<Lane> getLanes() {
         return lanes;
-    }
-
-    @Override
-    public void update(Time time) {
-        for (int i = 0; i < lanes.size(); i++){
-            for (Cell cell : lanes.get(i).getCells()) {
-                Tower tower = cell.getTower();
-                if (tower != null) {
-                    // if (tower.canAttack(getEnemiesFromStrategy(tower.getTargetingStrategy()))) {
-                    //      tower.attack();
-                    // }
-                    // ge mario avståndet till närmaste fiefacnde (eller kanske lista med fiender i lanen)
-                    // mario får svara om han kan attackera
-                    // om mario kan attackera så kallar lane på mario's attack metod
-
-                    // exempel:  if (mario.canAttack(enemies)) mario.attack()
-                }
-            }
-        }
     }
 }
