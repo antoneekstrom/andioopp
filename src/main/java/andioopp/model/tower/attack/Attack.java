905f9ae6a2f2719a1dc154c626ebfc2799670314
Elin <nileli@student.chalmers.se>
2021-09-21 11:03:30 +0200

    Adds method for checking if a tower is available for a new attack.

--- src/main/java/andioopp/model/tower/attack/Attack.java
+++ src/main/java/andioopp/model/tower/attack/Attack.java
@@ -1,9 +1,29 @@
 package andioopp.model.tower.attack;
 
+import andioopp.common.time.Time;
 import andioopp.model.tower.Tower;
 
 public abstract class Attack {
+    private final float coolDown;
+    private float timeSinceLastAttack;
+
+    public Attack(float coolDown) {
+        this.coolDown = coolDown;
+    }
+
     public abstract void performAttack(Tower tower);
-    public abstract boolean isAvailable();
+
+    public boolean isAvailableForAttack(Time time){
+        float deltaSeconds = time.getElapsedSeconds() - timeSinceLastAttack;
+        return(deltaSeconds > this.coolDown);
+    }
+
+    public void updateTimeSinceLastAttack(Time time) {
+        this.timeSinceLastAttack = time.getElapsedSeconds();
+    }
+
     public abstract AttackTargetArea getTargetArea();
+
+
 }
+
