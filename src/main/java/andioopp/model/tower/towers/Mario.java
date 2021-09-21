4512d83098073e391e4bb6211d864c2fcc0b5a89
Arvid Svedberg <arvid.sved@hotmail.com>
2021-09-21 12:04:35 +0200

    Adds isAttackable method without body

--- src/main/java/andioopp/model/tower/towers/Mario.java
+++ src/main/java/andioopp/model/tower/towers/Mario.java
@@ -3,12 +3,16 @@ package andioopp.model.tower.towers;
 import andioopp.model.enemy.Enemy;
 import andioopp.model.tower.TargetingStrategy;
 import andioopp.model.tower.Tower;
+import andioopp.model.tower.attack.Attack;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Arrays;
 
 
 public class Mario extends Tower {
+
+    ArrayList<Attack> list = new ArrayList();
 
     private static final String SPRITE_PATH = "mario.png";
     private static final int BASE_HEALTH = 7;
@@ -22,5 +26,13 @@ public class Mario extends Tower {
 
     public boolean canAttack(Collection<Enemy> enemies){
         return true;
+    }
+
+    @Override
+    public boolean isAttackable(Enemy enemy) {
+
+
+
+        return true; //true for now
     }
 }
