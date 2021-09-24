21dd80758618e0b3ce98820d0b09140e8bb07913
Elin <nileli@student.chalmers.se>
2021-09-24 13:49:10 +0200

    Adds properties for specific enemies

--- src/main/java/andioopp/model/enemy/enemies/Blooper.java
+++ src/main/java/andioopp/model/enemy/enemies/Blooper.java
@@ -1,4 +1,30 @@
+
 package andioopp.model.enemy.enemies;
 
-public class Blooper {
+import andioopp.common.time.Time;
+import andioopp.common.transform.ConcreteTransform;
+import andioopp.common.transform.TransformFactory;
+import andioopp.common.transform.Vector3f;
+import andioopp.model.Health;
+import andioopp.model.enemy.Enemy;
+import java.util.ArrayList;
+
+public class Blooper extends Enemy {
+
+    private static final String SPRITE_PATH = null;
+    private static final int BASE_HEALTH = 5;
+    private static final ArrayList<Enum> requirements = new ArrayList<>();
+    private static final TransformFactory transformFactory = ConcreteTransform.getFactory();
+
+    public Blooper(Vector3f position) {
+        super(SPRITE_PATH, transformFactory.createWithPosition(position), new Health(BASE_HEALTH));
+        //this.requirements.add(REQUIREMENT.WATER);
+    }
+
+    @Override
+    public void update(Time time) {
+        getTransform().translate(new Vector3f(-0.005f, 0, 0));
+    }
+
 }
+
