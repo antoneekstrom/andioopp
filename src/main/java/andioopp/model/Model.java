59a92ffc57f1be757e968ef6f4f24613bea56e81
Elin <nileli@student.chalmers.se>
2021-09-24 13:51:22 +0200

    Creates a Wavequeue with waves in the world.

--- src/main/java/andioopp/model/Model.java
+++ src/main/java/andioopp/model/Model.java
@@ -15,8 +15,8 @@ public class Model implements Updateable {
     private final ListFactory listFactory = new ArrayListFactory();
 
     public Model(WaveQueue waves, Player player) {
-        this.world = createWorld();
         this.waves = waves;
+        this.world = createWorld();
         this.player = player;
     }
 
@@ -31,11 +31,14 @@ public class Model implements Updateable {
 
         World world = builder.build();
         world.getCell(1, 3).setTower(Towers.mario());
-        world.addEnemy(Enemies.goomba(world, 0));
+        waves.addWavesToWaveQueue(world,1);
+        waves.addWaveToWorld(world);
+
+        /*world.addEnemy(Enemies.goomba(world, 0));
         world.addEnemy(Enemies.goomba(world, 1));
         world.addEnemy(Enemies.goomba(world, 2));
         world.addEnemy(Enemies.goomba(world, 3));
-        world.addEnemy(Enemies.goomba(world, 4));
+        world.addEnemy(Enemies.goomba(world, 4));*/
 
         return world;
     }
@@ -44,3 +47,6 @@ public class Model implements Updateable {
         return world;
     }
 }
+
+
+
--- src/main/java/andioopp/model/WaveQueue.java
+++ src/main/java/andioopp/model/WaveQueue.java
@@ -4,17 +4,46 @@ import andioopp.common.time.Time;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Random;
 
 public class WaveQueue {
+   /* Tasks:
+
+            - [ ]  Create wavequeue
+- [ ]  Make enemies come onto the gamefield at different times
+- [ ]  and in different lanes
+- [ ]  Make a wave consist of different enemies
+- [ ]  Fiender kan ha en svårighetsgrad? Så att de svåraste inte kommer i början kanske
+- [ ]*/
 
     private final Queue<Wave> queue;
+    Random rand = new Random();
 
     public WaveQueue() {
+
         queue = new LinkedList<>();
     }
 
     public Queue<Wave> getWaves() {
         return queue;
+    }
+
+    public void addWavesToWaveQueue(World world, int numWaves) {
+        Wave wave = new Wave(8);
+        for (int i = 0; i < numWaves; i++){
+            wave.addEnemyToWave(world);
+        }
+        queue.add(wave);
+
+    }
+    public void addWaveToWorld(World world){
+
+        Wave wave = queue.remove();
+        while(wave.enemyWave.size() != 0){
+            world.addEnemy(wave.enemyWave.remove());
+
+        }
+
     }
 
 }
