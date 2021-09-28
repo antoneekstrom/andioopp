c0cb3772e267aa1703b35e355db49f35c4490e02
Arvid Svedberg <arvid.sved@hotmail.com>
2021-09-28 09:24:11 +0200

    Adds buzzybeetle to enemy factory, Enemies.

--- src/main/java/andioopp/model/enemy/Enemies.java
+++ src/main/java/andioopp/model/enemy/Enemies.java
@@ -2,6 +2,7 @@ package andioopp.model.enemy;
 
 import andioopp.common.transform.Vector3f;
 import andioopp.model.World;
+import andioopp.model.enemy.enemies.BuzzyBeetle;
 import andioopp.model.enemy.enemies.Goomba;
 
 public class Enemies {
@@ -9,6 +10,11 @@ public class Enemies {
     public static Enemy goomba(World world, int row) {
         float worldEndX = world.getNumberOfCellsInLanes();
         return new Goomba(new Vector3f(worldEndX, row));
+    }
+
+    public static Enemy buzzyBeetle(World world, int row) {
+        float worldEndX = world.getNumberOfCellsInLanes();
+        return new BuzzyBeetle(new Vector3f(worldEndX, row));
     }
 
 }
