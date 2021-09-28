5d451cc7a8fe9af844dce21a918cd7cade108cd1
Arvid Svedberg <arvid.sved@hotmail.com>
2021-09-28 09:25:08 +0200

    Continues with tests of the attack filter

--- src/test/java/model/tower/TowerTest.java
+++ src/test/java/model/tower/TowerTest.java
@@ -7,33 +7,44 @@ import andioopp.model.enemy.Enemies;
 import andioopp.model.enemy.Enemy;
 import andioopp.model.tower.Tower;
 import andioopp.model.tower.Towers;
+import andioopp.model.tower.towers.Mario;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TowerTest {
 
     private Tower tower;
-    private Enemy enemy;
+    private Enemy goomba;
+    private Enemy buzzyBeetle;
     private Model model;
+    Tower mario = new Mario();
 
     @Before
     public void setup() {
+        System.out.println( mario.requirements.size() + " REQUIEMREMNT SIZE I TESTIFLEN");
         model = new Model(new WaveQueue(), new Player());
         World world = model.getWorld();
-        tower = Towers.mario();
-        enemy = Enemies.goomba(world, 1);
+        goomba = Enemies.goomba(world, 1);
+        buzzyBeetle = Enemies.buzzyBeetle(world, 1);
     }
 
     @Test
-    public void TestEnum() {
-        FilterRequirement e1 = FilterRequirement.GROUND;
-        FilterRequirement e2 = FilterRequirement.GROUND;
-
+    public void testtesttesttest() {
+        int i = goomba.requirements.size();
+        System.out.println(i + " Gomba req size");
+        int j = mario.requirements.size();
+        System.out.println(j + " mario req size");
     }
 
     @Test
-    public void TestFilter() {
-        boolean b = tower.checkFilters(enemy);
-        assertTrue(b);
+    public void TestFilterTrue() {
+        boolean s = mario.checkFilters(goomba);
+        assertTrue(s);
+    }
+
+    @Test
+    public void TestFilterFalse() {
+        boolean f = mario.checkFilters(buzzyBeetle);
+        assertFalse(f);
     }
 }
