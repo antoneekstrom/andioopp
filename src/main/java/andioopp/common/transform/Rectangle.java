ca2d180b3a7c8de5e35cb9c19065877eabd95ffd
Anton Ekström <anton.e.ekstrom@gmail.com>
2021-10-10 23:03:00 +0200

    Adds immutable and mutable variants of Rectangle

--- src/main/java/andioopp/common/transform/Rectangle.java
+++ src/main/java/andioopp/common/transform/Rectangle.java
@@ -1,57 +1,27 @@
 package andioopp.common.transform;
 
-import java.util.Objects;
+public interface Rectangle {
 
-public class Rectangle {
+    Vector3f getPosition();
 
-    private final Vector3f position;
-    private final Dimension size;
+    Rectangle setPosition(Vector3f position);
 
-    public Rectangle(Vector3f position, Dimension size) {
-        this.position = position;
-        this.size = size;
+    Dimension getSize();
+
+    Rectangle setSize(Dimension size);
+
+    default Rectangle set(Vector3f position, Dimension size) {
+        return setPosition(position).setSize(size);
     }
 
-    public Rectangle(float x, float y, float w, float h) {
-        this(new Vector3f(x, y), new Dimension(w, h));
-    }
-
-    public boolean contains(Vector3f point) {
+    default boolean contains(Vector3f point) {
         Vector3f min = getPosition();
         Vector3f max = getPosition().add(getSize().toVector());
         return point.getX() >= min.getX() && point.getX() <= max.getX() && point.getY() >= min.getY() && point.getY() <= max.getY();
     }
 
-    public Rectangle round() {
-        return new Rectangle(getPosition().round(), getSize().round());
+    default Rectangle round() {
+        return set(getPosition().round(), getSize().round());
     }
 
-    public Vector3f getPosition() {
-        return position;
-    }
-
-    public Dimension getSize() {
-        return size;
-    }
-
-    @Override
-    public String toString() {
-        return "Rectangle{" +
-                "position=" + position +
-                ", size=" + size +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Rectangle rectangle = (Rectangle) o;
-        return getPosition().equals(rectangle.getPosition()) && getSize().equals(rectangle.getSize());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getPosition(), getSize());
-    }
 }
