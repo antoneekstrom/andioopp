041e0a9567478467aa027d00b9723165cc1445b9
Amxnda <amanda.dyrell.papacosta@gmail.com>
2021-10-15 11:24:10 +0200

    Refactors service Co-authored-by: Jacob Bengtsson <jacob.bengtsson@rocketmail.com> Co-authored-by: Anton Ekström <anton.e.ekstrom@gmail.com> Co-authored-by: Arvid Svedberg <arvid.sved@hotmail.com> Co-authored-by: Elin Nilsson <991121elin@gmail.com>

--- src/main/java/andioopp/service/Service.java
+++
@@ -1,9 +0,0 @@
-package andioopp.service;
-
-import andioopp.common.time.Time;
-
-public interface Service<T> {
-    void onSetup(T context);
-    void onDestroy(T context);
-    void update(T context, Time time);
-}
