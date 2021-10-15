2c4b6fbd86d1d4678f6689c22586a1017715382c
Elin <nileli@student.chalmers.se>
2021-10-15 15:42:54 +0200

    Fixes error in DroppedCoinsView.

--- src/main/java/andioopp/view/views/world/DroppedCoinsView.java
+++ src/main/java/andioopp/view/views/world/DroppedCoinsView.java
@@ -2,6 +2,8 @@ package andioopp.view.views.world;
 
 import andioopp.common.graphics.Renderer;
 import andioopp.common.graphics.Sprite;
+import andioopp.common.math.Dimension;
+import andioopp.model.domain.enemy.Enemy;
 import andioopp.model.domain.entity.DroppedCoinEntity;
 import andioopp.model.Model;
 import andioopp.view.View;
@@ -14,7 +16,8 @@ public class DroppedCoinsView implements View<Model> {
         for (DroppedCoinEntity droppedCoin : model.getWorld().getDroppedCoins()) {
             ViewCoordinate viewCoordinate = viewport.getViewCoordinate(droppedCoin.getPosition());
             S sprite = renderer.getSpriteFactory().get("coin.jpg");
-            renderer.drawSprite(sprite, viewCoordinate);
+            Dimension<ViewCoordinate> viewSize = viewport.getViewSize(sprite.getSize());
+            renderer.drawSprite(sprite, viewCoordinate,viewSize);
         }
     }
 }
