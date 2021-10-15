36a6a2c0b7589640d17d6cf1808a69a10d0c2f15
Amxnda <amanda.dyrell.papacosta@gmail.com>
2021-10-15 11:24:10 +0200

    Refactors service
    Co-authored-by: Jacob Bengtsson <jacob.bengtsson@rocketmail.com>
    Co-authored-by: Anton Ekström <anton.e.ekstrom@gmail.com>
    Co-authored-by: Arvid Svedberg <arvid.sved@hotmail.com>
    Co-authored-by: Elin Nilsson <991121elin@gmail.com>

--- src/main/java/andioopp/service/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Services are split into domain and infrastructure.
- *
- * Infrastructure services provides controllers with abstractions for platform dependent functionality
- * such as persistence or resources.
- *
- * Domain services provide logic and functionality for interacting with the model.
- */
-package andioopp.service;
