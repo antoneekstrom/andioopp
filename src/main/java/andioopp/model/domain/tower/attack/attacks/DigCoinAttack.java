73f1fde771bc44ab8826d99d8719b07ebc1c30cd
Anton Ekström <anton.e.ekstrom@gmail.com>
2021-10-19 10:47:03 +0200

    Changes DigCoinAttack to drop coins instead of immediately inserting money into the players bank account

--- src/main/java/andioopp/model/domain/tower/attack/attacks/DigCoinAttack.java
+++ src/main/java/andioopp/model/domain/tower/attack/attacks/DigCoinAttack.java
@@ -4,11 +4,13 @@ import andioopp.common.javafx.time.FxClock;
 import andioopp.common.math.vector.Vector3f;
 import andioopp.common.math.range.FloatRange;
 import andioopp.model.Model;
+import andioopp.model.domain.entity.DroppedCoinEntity;
 import andioopp.model.domain.money.Money;
 import andioopp.model.domain.tower.attack.Attack;
 import andioopp.model.domain.damage.BaseDamageSource;
 import andioopp.model.domain.damage.DamageType;
 import andioopp.model.domain.tower.attack.strategies.NonTargeting;
+import andioopp.model.util.ModelCoordinate;
 
 /**
  * An attack (sort of) which generates money
@@ -25,7 +27,15 @@ public class DigCoinAttack extends Attack {
 
     @Override
     public void onAttack(Model model, Vector3f position) {
-        model.getPlayer().giveMoney(getRandomMoney());
+        spawnCoin(model, getCoinSpawnPosition(position), getRandomMoney());
+    }
+
+    private void spawnCoin(Model model, ModelCoordinate position, Money value) {
+        model.getWorld().getDroppedCoins().add(new DroppedCoinEntity(position, value));
+    }
+
+    private ModelCoordinate getCoinSpawnPosition(Vector3f origin) {
+        return new ModelCoordinate(origin.add(Vector3f.random().scale(0.5f)));
     }
 
     private Money getRandomMoney() {
