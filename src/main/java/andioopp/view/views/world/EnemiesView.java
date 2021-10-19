d98b0f5e90a2634a3be8a4a819c1e9f006f239ac
Anton Ekström <anton.e.ekstrom@gmail.com>
2021-10-19 11:01:12 +0200

    Fixes enemy positioning in view
    Co-authored-by: Jacob Bengtsson <jacob.bengtsson@rocketmail.com>

--- src/main/java/andioopp/view/views/world/EnemiesView.java
+++ src/main/java/andioopp/view/views/world/EnemiesView.java
@@ -1,8 +1,10 @@
 package andioopp.view.views.world;
 
+import andioopp.common.graphics.Color;
 import andioopp.common.graphics.Renderer;
 import andioopp.common.graphics.Sprite;
 import andioopp.common.math.dimension.Dimension;
+import andioopp.common.math.rectangle.ImmutableRectangle;
 import andioopp.common.math.vector.Vector3f;
 import andioopp.model.Model;
 import andioopp.model.domain.enemy.Enemy;
@@ -11,7 +13,7 @@ import andioopp.view.util.ModelViewport;
 
 public class EnemiesView implements View<Model> {
 
-    private static final float SIZE_PERCENTAGE_OF_CELL_HEIGHT = 1f;
+    private static final float SIZE_PERCENTAGE_OF_CELL_HEIGHT = 0.7f;
     private static final float OFFSET_FROM_CELL_BOTTOM = 0.4f;
 
     private final ModelViewport viewport;
@@ -31,11 +33,12 @@ public class EnemiesView implements View<Model> {
         S sprite = enemy.getSprite(renderer.getSpriteFactory());
 
         Dimension cellSize = viewport.getCellSize();
-        Dimension size = cellSize.scale(SIZE_PERCENTAGE_OF_CELL_HEIGHT);
+        Dimension size = sprite.getSize().setHeight(cellSize.getHeight() * SIZE_PERCENTAGE_OF_CELL_HEIGHT);
         Vector3f position = viewport.getPosition(enemy.getPosition());
-        Vector3f alignedWithCellBottom = position.add(cellSize.toVector().sub(size.toVector()));
+        Vector3f alignedWithCellBottom = position.add(cellSize.toVector().fromY().sub(size.toVector().fromY()));
         Vector3f withAddedOffset = alignedWithCellBottom.sub(cellSize.toVector().fromY().scale(OFFSET_FROM_CELL_BOTTOM));
 
         renderer.drawSprite(sprite, withAddedOffset, size);
+        renderer.drawRectangle(new ImmutableRectangle(position, new Dimension(5, 5)), new Color(255, 0, 0));
     }
 }
