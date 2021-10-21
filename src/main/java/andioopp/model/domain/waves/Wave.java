5b244ab956445f7b1a5f02159e98b6d22d2744bf
Elin <nileli@student.chalmers.se>
2021-10-21 15:47:14 +0200

    Refactors Wave, WaveQueue and UpdateWaveSystem.

--- src/main/java/andioopp/model/domain/waves/Wave.java
+++ src/main/java/andioopp/model/domain/waves/Wave.java
@@ -1,5 +1,6 @@
 package andioopp.model.domain.waves;
 
+import andioopp.common.time.Time;
 import andioopp.model.domain.enemy.EnemyFactory;
 import andioopp.model.domain.enemy.Enemy;
 import andioopp.model.domain.world.World;
@@ -9,31 +10,69 @@ import java.util.Random;
 import java.util.LinkedList;
 
 public class Wave {
+    float timeSinceLastEnemy;
+    float deltaSeconds;
+    private LinkedList<Enemy> enemyWave;
+    int difficulty;
 
-    public LinkedList<Enemy> enemyWave;
-    private final int numEnemies;
     Random rand = new Random();
 
 
-    public Wave(int numEnemies) {
-        this.numEnemies = numEnemies;
+    public Wave() {
+
         this.enemyWave = new LinkedList<>();
     }
-
+    public void createWave(World world, int numEnemies, int difficulty){
+        for(int i = 0; i< numEnemies; i++) {
+            addEnemyToWave(world, numEnemies);
+        }
+        setDifficulty(difficulty);
+    }
+    public LinkedList<Enemy> getEnemyWave(){
+        return enemyWave;
+    }
+    private void setDifficulty(int difficulty){
+        this.difficulty = difficulty;
+    }
     /**
      * Adds same amount as numEnemies of random enemies to Wave
      */
-    public void addEnemyToWave(World world) {
+    public void addEnemyToWave(World world, int numEnemies) {
         EnemyFactory enemies = new EnemyFactory();
         for (int i = 0; i < numEnemies; i++) {
             Enemy enemy = enemies.randomEnemy(world, rand.nextInt(world.getNumberOfLanes()));
-            Enemy enemy2 = enemies.randomEnemy(world, rand.nextInt(world.getNumberOfLanes()));
-            Enemy enemy3 = enemies.randomEnemy(world, rand.nextInt(world.getNumberOfLanes()));
-            Enemy enemy4 = enemies.randomEnemy(world, rand.nextInt(world.getNumberOfLanes()));
             enemyWave.add(enemy);
-            enemyWave.add(enemy2);
-            enemyWave.add(enemy3);
-            enemyWave.add(enemy4);
+
         }
+    }
+    /**
+     * Delays enemies so they don't appear on screen at the same time.
+     */
+    public boolean delayEnemies(Time time, double delay) {
+        this.deltaSeconds = time.getTime() - timeSinceLastEnemy;
+        return (this.deltaSeconds > delay);
+    }
+    public void setDeltaSeconds(float deltaSeconds) {
+        this.deltaSeconds = deltaSeconds;
+    }
+
+    public void updateTimeSinceLastEnemy(Time time) {
+        this.timeSinceLastEnemy = time.getTime();
+    }
+
+    /**
+     * Returns a random delay between 30 and 45.
+     */
+    public double getRandomDelay() {
+        int randomDelay = (rand.nextInt(10) + 10) / (difficulty +1);
+
+        return randomDelay; // * Math.pow(10,6.5);
+
+    }
+    /**
+     * Adds wave of enemies to the world
+     */
+    public void addEnemyFromWaveToWorld(World world) {
+        world.addEnemy(enemyWave.remove());
     }
 }
--- src/main/java/andioopp/model/system/systems/UpdateWaveSystem.java
+++ src/main/java/andioopp/model/system/systems/UpdateWaveSystem.java
@@ -2,8 +2,10 @@ package andioopp.model.system.systems;
 
 import andioopp.common.time.Time;
 import andioopp.model.Model;
+import andioopp.model.domain.waves.Wave;
 import andioopp.model.domain.waves.WaveQueue;
 import andioopp.model.system.System;
+
 
 public class UpdateWaveSystem implements System<Model> {
 
@@ -20,12 +22,14 @@ public class UpdateWaveSystem implements System<Model> {
     }
 
     private void updateWaves(Time time, Model model) {
-        if (waves.delayEnemies(time, delay)) {
-            if (waves.getWave().enemyWave.size() != 0) {
-                waves.addWaveToWorld(model.getWorld());
-                waves.setDeltaSeconds(0);
-                this.delay = waves.getRandomDelay();
-                waves.updateTimeSinceLastEnemy(time);
+        Wave wave = waves.getWave();
+
+        if (wave.delayEnemies(time, delay)) {
+            if (wave.getEnemyWave().size() != 0) {
+                wave.addEnemyFromWaveToWorld(model.getWorld());
+                wave.setDeltaSeconds(0);
+                this.delay = wave.getRandomDelay();
+                wave.updateTimeSinceLastEnemy(time);
             }
         }
     }
