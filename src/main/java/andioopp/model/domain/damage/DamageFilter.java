72e2c41dff90ddfb0eb973ba1aa20747ba01fea1
Anton Ekström <anton.e.ekstrom@gmail.com>
2021-10-24 11:44:58 +0200

    Changes DamageFilter to include methods isImmuneAgainst and meetsAllRequirements

--- src/main/java/andioopp/model/domain/damage/DamageFilter.java
+++ src/main/java/andioopp/model/domain/damage/DamageFilter.java
@@ -1,12 +1,37 @@
 package andioopp.model.domain.damage;
 
+import java.util.Collection;
+
 /**
- * A functional interface for DamageFilters.
+ * TODO
  */
-@FunctionalInterface
 public interface DamageFilter {
-    boolean canBeDamagedBy(DamageSource src);
-    default DamageFilter combineWith(DamageFilter other) {
-        return (src) -> canBeDamagedBy(src) && other.canBeDamagedBy(src);
+
+    boolean isImmuneAgainst(DamageSource src);
+
+    boolean meetsAllRequirements(DamageSource src);
+
+    default boolean isRequirementMet(Collection<DamageType> types, DamageType requirement) {
+        return types.contains(requirement);
     }
+
+    default boolean canBeDamagedBy(DamageSource src) {
+        return isRequirementMet(src.getTypes(), DamageType.ANY) || (!isImmuneAgainst(src) && meetsAllRequirements(src));
+    }
+
+    default DamageFilter combineWith(DamageFilter other) {
+        DamageFilter self = this;
+        return new DamageFilter() {
+            @Override
+            public boolean isImmuneAgainst(DamageSource src) {
+                return self.isImmuneAgainst(src) || other.isImmuneAgainst(src);
+            }
+
+            @Override
+            public boolean meetsAllRequirements(DamageSource src) {
+                return self.meetsAllRequirements(src) && other.meetsAllRequirements(src);
+            }
+        };
+    }
+
 }
